35b772f9154778e30f5638cde2302a2888958481
Aashish Ghimire <160713983+Ashish4721@users.noreply.github.com>
2025-10-08 00:43:46 +0545

    Update hero.jsx
    responsive

--- src/components/landing/hero.jsx
+++ src/components/landing/hero.jsx
@@ -17,19 +17,30 @@ const Hero = () => {
         <div className="flex-1 flex items-center justify-start ml-15">
           <div className="gap-5 flex flex-col">
             <div>
-              <h1 style={Fonts.poppins.medium} className=" text-2xl sm:text-3xl lg:text-5xl xl:text-7xl text-white ">
+          
+              <h1
+                style={Fonts.poppins.medium}
+                className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-7xl text-white"
+              >
                 Meditation for mind, body,
                 <br />
                 and soul with silent strength.
               </h1>
-              <h3 style={Fonts.poppins.regular} className="text-lg text-white pt-3">
+
+              <h3
+                style={Fonts.poppins.regular}
+                className="text-lg text-white pt-3"
+              >
                 We help you rediscover your inner peace through mindful
                 practices.
               </h3>
             </div>
 
             <div>
-              <button style={Fonts.poppins.regular} className="bg-[#000000] py-4 px-15 rounded-full text-white hover:bg-black duration-[800ms]">
+              <button
+                style={Fonts.poppins.regular}
+                className="bg-[#000000] py-4 px-15 rounded-full text-white hover:bg-black duration-[800ms]"
+              >
                 Try for free
               </button>
             </div>
@@ -38,14 +49,17 @@ const Hero = () => {
 
         {/* Bottom Contents */}
         <div className="flex flex-row items-end justify-between pb-8 pr-4 pl-4">
-          <div className="gap-3 flex-row items-center flex px-6 py-3 rounded-2xl  border-t border-l border-white/30 backdrop-blur-xs bg-black/15">
-            <img className="h-15 w-38" src={Users} />
-            <h3 style={Fonts.poppins.light} className="text-white ">
+          <div className="gap-3 flex-row items-center flex px-6 py-3 rounded-2xl border-t border-l border-white/30 backdrop-blur-xs bg-black/15">
+            <img className="h-15 w-38" src={Users} alt="users" />
+            <h3 style={Fonts.poppins.light} className="text-white">
               Trusted by 100+ users
             </h3>
           </div>
           <div className="py-6 px-10 bg-black/15 backdrop-blur-xs rounded-3xl border-t border-r border-white/30">
-            <h3 style={Fonts.poppins.light} className="text-white text-right">
+            <h3
+              style={Fonts.poppins.light}
+              className="text-white text-right"
+            >
               We help you cultivate calm, release stress, and create
               <br /> the mental and emotional space needed to invite
               <br /> clarity, balance, and well-being into every aspect of
